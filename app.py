e0b1b7c0cc640f84ee430e789fcb99fff7bf91f1
padlichan <lili.berenyi@gmail.com>
2025-12-04 17:16:07 +0000

    Add "controller" and view with e2e tests

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from repo.duty_repo import DutyRepo
+from services.duty_service import DutyService
+from services.results import AddDutyResult
+import os
+
+#type hints
+#linting - ruff an mypy
+#Add basic stuff to readme, like create venv
+#pycharm - IDE python instead of vs code
+#pip's garbage
+#use interface for differrnt repos
+#flask blueprints for endpoints
+
+repo = DutyRepo()
+service = DutyService(repo)
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.service = service
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    if request.method == "POST":
+        number = request.form.get("number")
+        number = int(number)
+        description = request.form.get("description")
+
+        result = service.add(number, description)
+        if result == AddDutyResult.EMPTY_DESCRIPTION:
+            session["error"]="Error: Description cannot be empty"
+        elif result == AddDutyResult.DUPLICATE:
+            session["error"]="Error: Duty already exists"
+        else:
+            session["error"]=None
+
+        duties = service.get_all()
+        return redirect(url_for('index'))
+
+    duties = app.service.get_all()
+    error_message = session.pop("error", None)
+    return render_template('index.html', duties=duties, error_message=error_message)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,6 +1,78 @@
 describe("Index page should exist", () => {
   it("Loads the index page", () => {
     cy.visit("http://localhost:5000");
-    cy.contains("Duty");
+    cy.contains("Duties");
+    cy.contains("Add Duty");
+  });
+});
+
+describe("Should be able to add a duty", () => {
+  it("Adds the duty", () => {
+    cy.visit("http://localhost:5000");
+    cy.get("input[name='number']").type("1");
+    cy.get("textarea[name='description']").type("Test duty");
+    cy.get("button[type='submit']").click();
+
+    cy.url().should("eq", "http://localhost:5000/");
+    cy.contains("Duty 1: Test duty");
+  });
+});
+
+describe("Should be able to add multiple duties", () => {
+  it("Displays error message when adding duplicate duty", () => {
+    cy.visit("http://localhost:5000");
+    cy.get("input[name='number']").type("2");
+    cy.get("textarea[name='description']").type("Test duty 2");
+    cy.get("button[type='submit']").click();
+
+    cy.get("input[name='number']").type("3");
+    cy.get("textarea[name='description']").type("Test duty 3");
+    cy.get("button[type='submit']").click();
+
+    cy.url().should("eq", "http://localhost:5000/");
+    cy.contains("Duty 2: Test duty 2");
+    cy.contains("Duty 3: Test duty 3");
+  });
+});
+
+describe("Shouldn't be able to add duplicate duty", () => {
+  it("Displays error message when adding duplicate duty", () => {
+    cy.visit("http://localhost:5000");
+    cy.get("input[name='number']").type("4");
+    cy.get("textarea[name='description']").type("Test duty 4");
+    cy.get("button[type='submit']").click();
+
+    cy.get("input[name='number']").type("4");
+    cy.get("textarea[name='description']").type("Test duty 5");
+    cy.get("button[type='submit']").click();
+
+    cy.url().should("eq", "http://localhost:5000/");
+    cy.contains("Duty already exists");
+  });
+});
+
+describe("Shouldn't be able to add duty with empty description", () => {
+  it("Displays error message when adding duty with empty description", () => {
+    cy.visit("http://localhost:5000");
+    cy.get("input[name='number']").type("5");
+    cy.get("button[type='submit']").click();
+
+    cy.url().should("eq", "http://localhost:5000/");
+    cy.contains("Error: Description cannot be empty");
+  });
+});
+
+describe("Shouldn't be able to add a duplicate duty with empty description", () => {
+  it("Displays correct error message when addingduplicate duty with empty description", () => {
+    cy.visit("http://localhost:5000");
+    cy.get("input[name='number']").type("6");
+    cy.get("textarea[name='description']").type("Test duty 6");
+    cy.get("button[type='submit']").click();
+
+    cy.get("input[name='number']").type("6");
+    cy.get("button[type='submit']").click();
+
+    cy.url().should("eq", "http://localhost:5000/");
+    cy.contains("Error: Description cannot be empty");
   });
 });
